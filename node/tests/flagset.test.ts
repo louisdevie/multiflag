c5878ef61ffe81806e212ece96c8660d40668c2e
Louis DEVIE <louisdevie.contact@gmail.com>
2025-04-28 23:13:53 +0200

    test(js): call flag() with arguments in the wrong order

--- node/tests/flagset.test.ts
+++ node/tests/flagset.test.ts
@@ -1,12 +1,19 @@
 import { NumberBitflagSet } from '@module'
-import { ForeignFlagError } from '../src/errors'
-import { FlagSet } from '../src/flagset'
 
 test('cannot create an abstract flag with less than two parents', () => {
     const flags = new NumberBitflagSet()
-    const flagA = flags.flag(1)
+    const flag1 = flags.flag(1)
 
     expect(() => flags.flag()).toThrow(TypeError)
-    expect(() => flags.flag(flagA)).toThrow(TypeError)
-    expect(() => flags.flag(flagA, flagA)).toThrow(TypeError)
+    expect(() => flags.flag(flag1)).toThrow(TypeError)
+    expect(() => flags.flag(flag1, flag1)).toThrow(TypeError)
+})
+
+test('calls to flag() with arguments in the wrong order throw a TypeError', () => {
+    const flags = new NumberBitflagSet()
+    const flag1 = flags.flag(1)
+    const flag2 = flags.flag(2)
+
+    // @ts-ignore
+    expect(() => flags.flag(flag1, 2, flag2)).toThrow(TypeError)
 })
